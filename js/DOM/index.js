03e8745566786fb0aef01e606a494656cc662e87
Abhishekkanu <abhishekkanu1922@gmail.com>
2025-09-29 12:52:37 +0530

    file import

--- js/DOM/index.js
+++ js/DOM/index.js
@@ -188,29 +188,61 @@
 
 
 
-//Promise
+// //Promise
 
-const mypromise = new Promise((resolve,reject)=>{
-const password = "786h%^";
-if(password.length>8){
-    resolve("password length is ok");
-}else{
-    reject("password length is not as per our policy");
+// const mypromise = new Promise((resolve,reject)=>{
+// const password = "786h%^";
+// if(password.length>8){
+//     resolve("password length is ok");
+// }else{
+//     reject("password length is not as per our policy");
+// }
+// });
+
+
+// // mypromise.then((result)=>{console.log(result)})
+// // .catch((err)=>(console.log(err)))
+// // .finally(()=>{
+// //     console.log("all the resources have closed successfully");
+// // })
+
+// async function handleData(){
+//     const data = await mypromise;
+//     console.log(data);
+//     // const result = mypromise;
+//     // console.log(result);
+// }
+
+// handleData();
+
+
+
+const button = document.getElementById('btn');
+const disp = document.getElementById('disp');
+let table =`<table border=1>`;
+async function fetchdata(){
+    disp.innerHTML="<h2>Data is loading...</h2>"
+    const response = await fetch('https://dummyjson.com/recipes');
+    const jsondata = await response.json();
+    console.log(jsondata.recipes);
+    // disp.innerHTML = `<h2>${jsondata.recipes[0].name} ${jsondata.recipes[0].id}</h2>`;
+
+    jsondata.recipes.forEach(element=>{
+        table+=`<tr>
+        <td><img src=${element.image} height=200 width=200 alt=/image></td>
+        <td>${element.id}</td>
+        <td>${element.name}</td>
+        <td>${element.ingredients}</td>
+        <td></td>
+        </tr>`
+    });
+
+    table+='</table>';
+    disp.innerHTML=table;
+
 }
-});
+button.addEventListener('click',fetchdata);
 
 
-// mypromise.then((result)=>{console.log(result)})
-// .catch((err)=>(console.log(err)))
-// .finally(()=>{
-//     console.log("all the resources have closed successfully");
-// })
 
-async function handleData(){
-    const data = await mypromise;
-    console.log(data);
-    // const result = mypromise;
-    // console.log(result);
-}
 
-handleData();
